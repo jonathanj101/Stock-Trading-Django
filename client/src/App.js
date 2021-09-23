9f447b8ec9201b16d66882ea0a0798f453df3fea
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-22 22:20:58 -0400

    react app initialized

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./Components/Navbar/Navbar";
+import Home from "./Components/Pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
