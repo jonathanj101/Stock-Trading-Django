40c5c2e9b7e367a5a0688aefa054527d472c9a63
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-25 23:55:20 -0400

    From Func() to Class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,108 @@
+import React, { Component } from "react"
 import "./App.css";
 import Navigation from "./Components/Navbar/Navbar";
 import Home from "./Components/Pages/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Home />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      userId: '',
+      userHoldings: '',
+      isLogged: false
+    }
+  }
+
+  //   async componentDidMount() {
+  //     const localStorageUserId = JSON.parse(localStorage.getItem('userId'));
+  //     if (localStorageUserId !== null) {
+  //         const response = await axios
+  //             .post('/user', {
+  //                 id: localStorageUserId,
+  //             })
+  //             .then((data) => {
+  //                 return data.data;
+  //             });
+  //         this.setState({
+  //             userId: localStorageUserId,
+  //             username: response.username,
+  //             userHoldings: response.user_holdings,
+  //             isLogged: true,
+  //         });
+  //     } else {
+  //         return;
+  //     }
+  // }
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //     const localStorageUserId = JSON.parse(localStorage.getItem('userId'));
+  //     if (this.state.userHoldings !== prevState.userHoldings) {
+  //         const response = await axios
+  //             .post('/user', {
+  //                 id: localStorageUserId,
+  //             })
+  //             .then((data) => {
+  //                 return data.data;
+  //             });
+  //         this.setState({
+  //             id: localStorageUserId,
+  //             username: response.username,
+  //             userHoldings: response.user_holdings,
+  //         });
+  //     }
+  // }
+
+  isUserAuthenticated = () => {
+    const localStorageUserId = JSON.parse(localStorage.getItem('userId'));
+    if (localStorageUserId !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  handleRegister = (userId, username) => {
+    if (userId !== undefined) {
+      this.setState({
+        userId: userId,
+        userHoldings: 100000,
+        username: username,
+        isLogged: true,
+      });
+    } else {
+      return;
+    }
+  };
+
+  handleLogIn = (userId, username) => {
+    debugger;
+    if (userId) {
+      localStorage.setItem('userId', JSON.stringify(userId));
+      this.setState({
+        userId: userId,
+        username: username,
+        isLogged: true,
+      });
+    }
+    return;
+  };
+
+  handleLogOut = () => {
+    this.setState({
+      username: '',
+      isLogged: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App" >
+        <Navigation isLogged={this.state.isLogged} />
+        <Home />
+      </div>
+    )
+  }
 }
 
 export default App;
