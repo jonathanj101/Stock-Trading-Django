e3fc30459a222a56596d726ecc5923f7cef0e3da
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-26 22:10:52 -0400

    page not found compnent added

---
+++ client/src/Components/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div id="page-not-found-component">
+            <div>
+                <h1>Page not found</h1>
+                <p>Click button below to take you to home page</p>
+                <button href="/">Home</button>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound
