150f7b76ccf1b1bd262641d83692100ea2916f84
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-27 23:02:50 -0400

    search component not showing in /, not sure why

--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Button, Card } from "react-bootstrap"
 import axios from 'axios'
+import SearchComponent from "../Search-Component/SearchComponent";
 
 const Home = () => {
 
     return (
-        <div id="home">
+        <div id="home" style={{ padding: "20px" }}>
+            <SearchComponent />
         </div>
     );
 };
