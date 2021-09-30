0561fbe4c937ffa9dd93d408dfbb6a302f813f4b
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-29 21:36:55 -0400

    testing stock history url, working

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.urls import path
-from .views import fetch_news, search_stock
+from .views import fetch_news, search_stock, test
 
 
 urlpatterns = [
     path("news", fetch_news),
-    path("search_stock/<str:stock>", search_stock)
+    path("search_stock/<str:stock>", search_stock),
+    path("test/<str:stock>", test)
     ]
