982a9f689d9d64d243f5c5c32a190b52ed778890
Jonathan Jimenez <jonatjimen@gmail.com>
2021-09-29 21:37:13 -0400

    testing logic for stock history, working

--- api/views.py
+++ api/views.py
@@ -43,42 +43,53 @@ def search_stock(request, stock):
     except ValueError:
         return Response("Stock symbol/s not found!")
 
+@api_view(["GET"])
+def test(request,stock):
+    print(stock)
+    print(IEX_CLOUD_API_KEY)
+    search_query = f"{BASE_URL}/stable/stock/{stock}/chart/1m?token={IEX_CLOUD_API_KEY}"
+    request = requests.get(search_query)
+    print(request)
+    response = request.json()
+
+    return Response(response)
+
 # @api_view(["POST"])
 # def add_stock(request):
-    USER_DETAILS = request.get_json()
-    USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
-    if USER:
-        STOCK = Stock.query.filter_by(
-            stock_symbol=USER_DETAILS["stockSymbol"], user_id=USER_DETAILS["id"]).first()
-        if STOCK != None:
+    # USER_DETAILS = request.get_json()
+    # USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
+    # if USER:
+    #     STOCK = Stock.query.filter_by(
+    #         stock_symbol=USER_DETAILS["stockSymbol"], user_id=USER_DETAILS["id"]).first()
+    #     if STOCK != None:
 
-            update_user_cost = (STOCK.user_estimated_cost +
-                                USER_DETAILS['estimatedCost'])
-            update_user_shares = (STOCK.user_estimated_shares +
-                                  USER_DETAILS['estimatedShares'])
-            update_user_holdings = USER.user_holdings - update_user_cost
-            filter_by_stock_symbol.user_estimated_cost = update_user_cost
-            filter_by_stock_symbol.user_estimated_shares = update_user_shares
-            USER.user_holdings = update_user_holdings
-            transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS['estimatedCost'],
-                                       user_holdings=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
-            db.session.add(transaction)
-            db.session.commit()
-            return jsonify("Success! Stock added!"), 200
-        else:
-            user_holdings = USER.user_holdings - USER_DETAILS['estimatedCost']
-            USER.user_holdings = user_holdings
-            user_stock = Stock(company_name=USER_DETAILS['company_name'],
-                               stock_symbol=USER_DETAILS['stockSymbol'], stock_cost=USER_DETAILS['stockCost'],
-                               user_estimated_shares=USER_DETAILS['estimatedShares'], user_estimated_cost=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
-            transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS[
-                'estimatedCost'], user_holdings=user_holdings, user_id=USER_DETAILS['id'])
-            db.session.add(user_stock)
-            db.session.add(transaction)
-            db.session.commit()
-            return jsonify("Success! Stock updated!"), 200
-    else:
-        return jsonify('Something went wrong on our end! Please try again later.'), 500
+    #         update_user_cost = (STOCK.user_estimated_cost +
+    #                             USER_DETAILS['estimatedCost'])
+    #         update_user_shares = (STOCK.user_estimated_shares +
+    #                               USER_DETAILS['estimatedShares'])
+    #         update_user_holdings = USER.user_holdings - update_user_cost
+    #         filter_by_stock_symbol.user_estimated_cost = update_user_cost
+    #         filter_by_stock_symbol.user_estimated_shares = update_user_shares
+    #         USER.user_holdings = update_user_holdings
+    #         transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS['estimatedCost'],
+    #                                    user_holdings=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
+    #         db.session.add(transaction)
+    #         db.session.commit()
+    #         return jsonify("Success! Stock added!"), 200
+    #     else:
+    #         user_holdings = USER.user_holdings - USER_DETAILS['estimatedCost']
+    #         USER.user_holdings = user_holdings
+    #         user_stock = Stock(company_name=USER_DETAILS['company_name'],
+    #                            stock_symbol=USER_DETAILS['stockSymbol'], stock_cost=USER_DETAILS['stockCost'],
+    #                            user_estimated_shares=USER_DETAILS['estimatedShares'], user_estimated_cost=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
+    #         transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS[
+    #             'estimatedCost'], user_holdings=user_holdings, user_id=USER_DETAILS['id'])
+    #         db.session.add(user_stock)
+    #         db.session.add(transaction)
+    #         db.session.commit()
+    #         return jsonify("Success! Stock updated!"), 200
+    # else:
+    #     return jsonify('Something went wrong on our end! Please try again later.'), 500
 
 # @api_view(["POST"])
 # def sell_stock():
