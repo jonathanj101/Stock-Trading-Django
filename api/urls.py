345b1efb5b9f0d327c76befd589ee85601b3888e
= <=>
2021-10-03 23:46:42 -0400

    adding signup route

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls import path
-from .views import fetch_news, search_stock, test
+from .views import fetch_news, search_stock, signup, test
 
 
 urlpatterns = [
     path("news", fetch_news),
     path("search_stock/<str:stock>", search_stock),
-    path("test/<str:stock>", test)
+    path("test/<str:stock>", test),
+    path("signup", signup)
     ]
