68aa344b448737e7ae9caea738a38776abd4a157
jonathanj101 <jonatjimen@gmail.com>
2021-10-04 21:22:28 -0400

    user,add routes handled

--- api/views.py
+++ api/views.py
@@ -19,21 +19,19 @@ BASE_URL = "https://cloud.iexapis.com"
 
 @api_view(["GET"])
 def fetch_news(request):
-    print(request)
     url = f"{BASE_URL}/stable/time-series/news?range=1m&limit=30&token={IEX_CLOUD_API_KEY}"
     make_request = requests.get(url)
     response = make_request.json()
-    # print(make_request)
 
     return Response(response)
 
 @api_view(["GET"])
 def search_stock(request, stock):
-
     try:
         URL = f"{BASE_URL}/stable/stock/{stock}/quote?token={IEX_CLOUD_API_KEY}"
         request = requests.get(URL)
         response = request.json()
+
         data = {
             "company_name": response["companyName"],
                 "cost": response["latestPrice"],
@@ -45,45 +43,50 @@ def search_stock(request, stock):
     except ValueError:
         return Response("Stock symbol/s not found!")
 
-@api_view(["GET"])
-def test(request,stock):
-    print(stock)
-    print(IEX_CLOUD_API_KEY)
-    search_query = f"{BASE_URL}/stable/stock/{stock}/chart/1m?token={IEX_CLOUD_API_KEY}"
-    request = requests.get(search_query)
-    print(request)
-    response = request.json()
 
-    return Response(response)
+@api_view(["POST"])
+def add_stock(request):
+    DATA_RECIEVED = request.data
+    FILTER_BY_USER = User.objects.filter(username=DATA_RECIEVED["username"]).first()
+    FILTER_BY_STOCK = Stock.objects.filter(stock_symbol=DATA_RECIEVED["stockSymbol"], user_id_id=FILTER_BY_USER.id).first()
 
-# @api_view(["POST"])
-# def add_stock(request):
-    # USER_DETAILS = request.get_json()user
-    #     STOCK = Stock.query.filter_by(
-    #         stock_symbol=USER_DETAILS["stockSymbol"], user_id=USER_DETAILS["id"]).first()
-    #     if STOCK != None:
+    print(FILTER_BY_USER)
+    print(FILTER_BY_USER.id)
+    print(FILTER_BY_STOCK)
 
-    #         update_user_cost = (STOCK.user_estimated_cost +
-    #                             USER_DETAILS['estimatedCost'])
-    #         update_user_shares = (STOCK.user_estimated_shares +
-    #                               USER_DETAILS['estimatedShares'])
-    #         update_user_holdings = USER.user_holdings - update_user_cost
-    #         filter_by_stock_symbol.user_estimated_cost = update_user_cost
-    #         filter_by_stock_symbol.user_estimated_shares = update_user_shares
-    #         USER.user_holdings = update_user_holdings
-    #         transaction = Transactions(company_name=USER_DETAILS['company_name'], user_estimated_cost=USER_DETAILS['estimatedCost'],
-    #                                    user_holdings=USER_DETAILS['estimatedCost'], user_id=USER_DETAILS['id'])
-    #         db.session.add(transaction)
-    #         db.session.commit()
-    #         return jsonify("Success! Stock added!"), 200
-    #     else:
-    #         user_holdings = USER.user_holdings - USER_DETAILS['estimatedCost']
-    #         USER.user_holdings = user_holdings
-    #         user_stock = Stock(company_name=USER_DETAILS['company_name'],
-    #                            stock_symbol=USER_DETAILS['stockSymuser
-    #         return jsonify("Success! Stock updated!"), 200
-    # else:
-    #     return jsonify('Something went wrong on our end! Please try again later.'), 500
+    if FILTER_BY_USER:
+
+        if FILTER_BY_STOCK != None:
+
+            update_user_cost = (FILTER_BY_STOCK.user_estimated_cost + DATA_RECIEVED["estimatedCost"])
+            update_user_shares = (FILTER_BY_STOCK.user_estimated_shares + DATA_RECIEVED["estimatedShares"])
+            update_user_holdings = FILTER_BY_USER.user_holdings - update_user_cost
+
+            FILTER_BY_STOCK.user_estimated_cost = update_user_cost
+            FILTER_BY_STOCK.user_estimated_shares = update_user_shares
+            FILTER_BY_USER.user_holdings = update_user_holdings
+
+
+            transaction = Transactions(company_name=DATA_RECIEVED["companyName"], user_estimated_cost=DATA_RECIEVED ["estimatedCost"],user_holdings=FILTER_BY_USER.user_holdings, user_id_id=FILTER_BY_USER.id)
+
+            transaction.save()
+
+            return Response({"message":"Success! Stock added!", "status_code":201})
+        else:
+            user_holdings = FILTER_BY_USER.user_holdings - DATA_RECIEVED['estimatedCost']
+            print(user_holdings)
+            FILTER_BY_USER.user_holdings = user_holdings
+            user_stock = Stock(company_name=DATA_RECIEVED['companyName'],
+                               stock_symbol=DATA_RECIEVED['stockSymbol'], stock_cost=DATA_RECIEVED['stockCost'],
+                               user_estimated_shares=DATA_RECIEVED['estimatedShares'], user_estimated_cost=DATA_RECIEVED['estimatedCost'], user_id_id=FILTER_BY_USER.id)
+            transaction = Transactions(company_name=DATA_RECIEVED['companyName'], user_estimated_cost=DATA_RECIEVED[
+                'estimatedCost'], user_holdings=user_holdings, user_id_id=FILTER_BY_USER.id)
+            user_stock.save()
+            transaction.save()
+            
+            return Response({"message":"Success! Stock updated!", "status_code":201})
+
+    return Response({"message":"Something went wrong on out end! Please try again!", "status_code":500})
 
 # @api_view(["POST"])
 # def sell_stock():
@@ -207,18 +210,13 @@ def signup(request):
 #         MESSAGE = "We don't recognize that username or password. Please try again!"
 #         return jsonify(MESSAGE, 500)
 
-# @api_view(["PUT"])
-# def user():
-#     USER_DETAILS = request.get_json()
-
-#     USER = Users.query.filter_by(id=USER_DETAILS['id']).first()
-
-#     user_obj = {
-#         "username": USER.username,
-#         "user_holdings": USER.user_holdings
-#     }
-#     if USER:
-
-#         return jsonify(user_obj), 200
-#     else:
-#         return ("User not found!"), 500
+@api_view(["PUT"])
+def user(request):
+    USER_DETAILS = request.data
+    USER = User.objects.filter(username=USER_DETAILS["username"]).first()
+    print(USER_DETAILS)
+    print(USER)
+    if USER:
+        return Response({"username": USER.username, "user_holdings":USER.user_holdings},201)
+    else: 
+        return Response({"message":"User not found on our database","status_code": 500})
