fb5a6b02040c154c736ace2d188fe45500e9905a
jonathanj101 <jonatjimen@gmail.com>
2021-10-04 21:10:22 -0400

    added banner component, for welcoming user etc

--- client/src/Components/Banner-Component/Banner.js
+++ client/src/Components/Banner-Component/Banner.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Banner = () => {
     return (
-        <div id="banner-component" style={{ width: "50%", margin: "auto" }}>
+        <div id="banner-component" style={{ width: '50%', margin: 'auto' }}>
             <div>
-                <p style={{ fontSize: "2.75rem", width: "fit-content", margin: "auto" }}>Start Investing</p>
+                <p
+                    style={{
+                        fontSize: '2.75rem',
+                        width: 'fit-content',
+                        margin: 'auto',
+                    }}
+                >
+                    Start Investing
+                </p>
             </div>
-            <div style={{ textAlign: "center" }}>
-                <p style={{ fontSize: "1.25rem" }}>If not registered, what are you waiting for?</p>
-                <p style={{ fontSize: "1.25rem" }}>Create an account now</p>
-                <p style={{ fontSize: "1.25rem" }}>And start investing with no real corrency, worry free!</p>
+            <div style={{ textAlign: 'center' }}>
+                <p style={{ fontSize: '1.25rem' }}>
+                    If not registered, what are you waiting for?
+                </p>
+                <p style={{ fontSize: '1.25rem' }}>Create an account now</p>
+                <p style={{ fontSize: '1.25rem' }}>
+                    And start investing with no real currency, worry free!
+                </p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Banner
+export default Banner;
