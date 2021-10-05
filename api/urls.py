931064888745ad748d7551eb4a43d0746981042f
jonathanj101 <jonatjimen@gmail.com>
2021-10-04 21:21:46 -0400

    added path for search, buy,signup,user routes

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.urls import path
-from .views import fetch_news, search_stock, signup, test
+from .views import add_stock, fetch_news, search_stock, signup, user
 
 
 urlpatterns = [
     path("news", fetch_news),
-    path("search_stock/<str:stock>", search_stock),
-    path("test/<str:stock>", test),
-    path("signup", signup)
+    path("search/<str:stock>", search_stock),
+    path("buy",add_stock),
+    path("signup", signup),
+    path('user', user)
     ]
