6d40cfc225b9431937f679344371cf196b0c0b31
jonathanj101 <jonatjimen@gmail.com>
2021-10-08 12:33:17 -0400

    testing sell logic still

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.urls import path
-from .views import add_stock, fetch_news, search_stock, signup, user, user_stock
+from .views import add_stock, fetch_news, search_stock, sell_stock, signup, user, user_stock
 
 
 urlpatterns = [
     path("news", fetch_news),
     path("search/<str:stock>", search_stock),
     path("buy",add_stock),
+    path("sell-stock", sell_stock),
     path("signup", signup),
     path('user', user),
-    path("stocks", user_stock)
+    path("stocks", user_stock),
     # path("stocks/<str:username>", user_stock)
     ]
