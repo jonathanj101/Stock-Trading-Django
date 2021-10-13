eff97fe71f331e7834e4737932a50c6ca960fa04
jonathanj101 <jonatjimen@gmail.com>
2021-10-13 16:52:29 -0400

    allsell logic moved to investing table component

--- client/src/Components/Pages/Summary.js
+++ client/src/Components/Pages/Summary.js
@@ -1,108 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
 import axios from 'axios';
-import Sidebar from '../SideBar-Content/Sidebar';
-import SearchComponent from '../../Components/Search-Component/SearchComponent';
-import SellStockModal from '../Buy-Sell-Handlers/Sell-Stock/SellStockModal';
 import { StockChart } from '../Chart/StockChart';
+import News from '../News-Stream/News';
+import InvestingTable from '../Investing/InvestingTable';
 
 const Summary = () => {
-    const [companyName, setCompanyName] = useState('');
-    const [stockSymbol, setStockSymbol] = useState('');
-    const [stockCost, setStockCost] = useState('');
-    const [estimatedShares, setEstimatedShares] = useState('');
-    const [estimatedCost, setEstimatedCost] = useState('');
-    const [differenceInCost, setDifferenceInCost] = useState('');
-    const [userHoldings, setUserHoldings] = useState('');
-    const [isSellModal, setIsSellModal] = useState(false);
-    const [items, setItems] = useState([]);
+    const [stocksData, setStocksData] = useState([]);
 
-    const fetchSome = async () => {
-        const response = await axios.get('http://127.0.0.1:8000/api/stocks');
-        console.log(response);
-
-        setItems(response.data);
-    };
-
-    const getStockFromSearchAddToModal = (
-        companyName,
-        stockSymbol,
-        stockCost,
-    ) => {
-        console.log(
-            ` summary component ${companyName}, ${stockSymbol}, ${stockCost}`,
+    useEffect(() => {
+        const localStorageUsername = JSON.parse(
+            localStorage.getItem('username'),
         );
-        // setCompanyName(companyName)
-        // setStockSymbol(stockSymbol)
-        // setStockCost(stockCost)
-    };
-
-    const handleStockInfoOnClick = (stock) => {
-        console.log(stock);
-        setCompanyName(stock.companyName);
-        setStockSymbol(stock.symbol);
-        setStockCost(stock.cost);
-        setUserHoldings(stock.userHoldings);
-        setEstimatedShares(stock.userEstimatedShares);
-        setEstimatedCost(stock.userEstimatedHolding);
-        setDifferenceInCost(stock.differenceInCost);
-    };
-
-    const handleCloseSellModal = () => {
-        setIsSellModal(false);
-        setCompanyName('');
-        setStockSymbol('');
-        setStockCost('');
-        setUserHoldings('');
-        setEstimatedShares('');
-        setEstimatedCost('');
-        setDifferenceInCost('');
-    };
-
-    const userList = items.map((item, num) => {
-        return (
-            <div>
-                <Card style={{ width: '250px', height: '250px' }} key={num}>
-                    <Card.Body>
-                        <Card.Text>{item.companyName}</Card.Text>
-                        <Button
-                            onClick={() => {
-                                handleStockInfoOnClick(item);
-                                setIsSellModal(true);
-                            }}
-                        >
-                            Sell
-                        </Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        );
-    });
+        const fetchUserStocks = async () => {
+            const response = await axios.put(
+                'http://127.0.0.1:8000/api/stocks',
+                {
+                    username: localStorageUsername,
+                },
+            );
+            setStocksData(response.data.data);
+        };
+        fetchUserStocks();
+    }, []);
 
     return (
         <div id="summary-component">
-            <SellStockModal
-                handleCloseSellModal={handleCloseSellModal}
-                showSellStockModal={isSellModal}
-                stockName={companyName}
-                stockSymbol={stockSymbol}
-                userHoldings={userHoldings}
-                stockCost={stockCost}
-                estimatedShares={estimatedShares}
-                estimatedCost={estimatedCost}
-                differenceInCost={differenceInCost}
-            />
-
-            <SearchComponent
-                getStockFromSearchAddToModal={getStockFromSearchAddToModal}
-            />
             <StockChart />
-            <Sidebar />
-            {items.length === 0 ? (
-                <button onClick={() => fetchSome()}>click</button>
-            ) : (
-                userList
-            )}
+            <InvestingTable data={stocksData} />
+            <News />
         </div>
     );
 };
