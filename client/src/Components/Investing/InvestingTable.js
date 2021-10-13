e538ffad558b7f8ec6c98d15ccdd92d26d1499fe
jonathanj101 <jonatjimen@gmail.com>
2021-10-13 16:49:31 -0400

    displaying all users investing stocks in a table

---
+++ client/src/Components/Investing/InvestingTable.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { Table } from 'react-bootstrap';
+import SellStockModal from '../Buy-Sell-Handlers/Sell-Stock/SellStockModal';
+
+const InvestingTable = ({ data }) => {
+    const [companyName, setCompanyName] = useState('');
+    const [stockSymbol, setStockSymbol] = useState('');
+    const [stockCost, setStockCost] = useState('');
+    const [estimatedShares, setEstimatedShares] = useState('');
+    const [estimatedCost, setEstimatedCost] = useState('');
+    const [differenceInCost, setDifferenceInCost] = useState('');
+    const [userHoldings, setUserHoldings] = useState('');
+    const [isSellModal, setIsSellModal] = useState(false);
+
+    const handleStockInfoOnClick = (stock) => {
+        setCompanyName(stock.companyName);
+        setStockSymbol(stock.symbol);
+        setStockCost(stock.cost);
+        setUserHoldings(stock.userHoldings);
+        setEstimatedShares(stock.userEstimatedShares);
+        setEstimatedCost(stock.userEstimatedHolding);
+        setDifferenceInCost(stock.differenceInCost);
+    };
+
+    const handleCloseSellModal = () => {
+        setIsSellModal(false);
+        setCompanyName('');
+        setStockSymbol('');
+        setStockCost('');
+        setUserHoldings('');
+        setEstimatedShares('');
+        setEstimatedCost('');
+        setDifferenceInCost('');
+    };
+
+    const tableRows = data.map((stock, num) => {
+        return (
+            <tr
+                key={num}
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Want to sell? Just click!"
+                className="tableRow"
+                onClick={() => {
+                    handleStockInfoOnClick(stock);
+                    setIsSellModal(true);
+                }}
+                style={{
+                    fontWeight: 'bold',
+                }}
+            >
+                <td>
+                    <span>{stock.companyName}</span>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div style={{ margin: '50px auto' }}>
+            <SellStockModal
+                handleCloseSellModal={handleCloseSellModal}
+                showSellStockModal={isSellModal}
+                stockName={companyName}
+                stockSymbol={stockSymbol}
+                userHoldings={userHoldings}
+                stockCost={stockCost}
+                estimatedShares={estimatedShares}
+                estimatedCost={estimatedCost}
+                differenceInCost={differenceInCost}
+            />
+            <Table
+                style={{ width: '50%', margin: 'auto', textAlign: 'center' }}
+                striped
+                bordered
+                hover
+            >
+                <thead>
+                    <tr>
+                        <th>
+                            <h3>Company Name</h3>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.length === 0 ? (
+                        <tr>
+                            <h1>GO BIG OR GO HOME!</h1>
+                        </tr>
+                    ) : (
+                        tableRows
+                    )}
+                </tbody>
+            </Table>
+        </div>
+    );
+};
+
+export default InvestingTable;
