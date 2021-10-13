6f26e2771dfac532b47ba373be5fa88369ee7e59
jonathanj101 <jonatjimen@gmail.com>
2021-10-13 16:51:49 -0400

    some styling issues

--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import { Button, Card } from "react-bootstrap"
-import News from "../../Components/News-Stream/News"
-import Banner from "../Banner-Component/Banner";
+import React, { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import News from '../../Components/News-Stream/News';
+import Banner from '../Banner-Component/Banner';
 
 const Home = () => {
-
     return (
-        <div id="home" style={{ height: "1000px", width: "100%", margin: "100px auto" }}>
+        <div
+            id="home"
+            style={{ height: '100%', width: '100%', margin: '100px auto' }}
+        >
             <Banner />
+            {/* <div style={{ margin: '100px auto' }}> */}
             <News />
         </div>
     );
