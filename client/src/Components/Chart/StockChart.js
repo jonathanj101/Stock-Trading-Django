9c9cfcf7fd5fc5663b2167220c4cb32d7d7ea39c
jonathanj101 <jonatjimen@gmail.com>
2021-10-12 23:51:13 -0400

    testing some logic to display user investing ammount but not working

--- client/src/Components/Chart/StockChart.js
+++ client/src/Components/Chart/StockChart.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
-import { Chart } from "react-google-charts"
-import axios from "axios"
+import React, { useState } from 'react';
+import { Chart } from 'react-google-charts';
+import axios from 'axios';
 
 export const StockChart = () => {
-
-    const [stock, setData] = useState([])
+    const [stock, setData] = useState([]);
 
     const fetchHistory = async () => {
-        const stock = "aapl"
-        const response = await axios.get(`http://127.0.0.1:8000/api/test/${stock}`)
-        console.log(response)
-        setData(response.data)
-
-    }
+        // debugger
+        // const response = await axios.get('http://127.0.0.1:8000/api/stocks');
+        // console.log(response);
+        const stock = 'aapl';
+        const response = await axios.get(
+            `http://127.0.0.1:8000/api/stocks/${stock}`,
+        );
+        console.log(response);
+        setData(response.data);
+    };
 
     return (
         <div>
@@ -24,10 +27,10 @@ export const StockChart = () => {
                 loader={<div>Loading Chart</div>}
                 data={[
                     ['Date', 'Price'],
-                    ...stock.map(stock => [stock.date, stock.close])
+                    ...stock.map((stock) => [stock.date, stock.close]),
                 ]}
                 options={{
-                    title: 'Company Performance',
+                    title: 'Investing',
                     hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
                     vAxis: { minValue: 0 },
                     // For the legend to fit, we make the chart area smaller
@@ -35,7 +38,6 @@ export const StockChart = () => {
                     // lineWidth: 25
                 }}
             />
-
         </div>
-    )
-}
+    );
+};
