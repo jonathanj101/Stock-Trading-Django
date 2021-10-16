a16e26d6c203fd05b2885a701af0c82302f8dfcd
jonathanj101 <jonatjimen@gmail.com>
2021-10-15 23:55:49 -0400

    added styling

--- client/src/Components/PageNotFound.js
+++ client/src/Components/PageNotFound.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const PageNotFound = () => {
     return (
-        <div id="page-not-found-component">
-            <div>
+        <div
+            id="page-not-found-component"
+            style={{ height: '100%', width: '100%', margin: '300px auto' }}
+        >
+            <div style={{ width: '50%', margin: 'auto', textAlign: 'center' }}>
                 <h1>Page not found</h1>
                 <p>Click button below to take you to home page</p>
-                <button href="/">Home</button>
+                <Button href="/">Home</Button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PageNotFound
+export default PageNotFound;
